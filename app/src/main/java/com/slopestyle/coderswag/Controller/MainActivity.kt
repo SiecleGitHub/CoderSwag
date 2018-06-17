7b4bc489700d689726a9b70cbaaeec4dc9fe63a9
clemenssietas <siecle@gmx.de>
2018-06-17 21:47:07 +0200

    lesson 02

--- app/src/main/java/com/slopestyle/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/slopestyle/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.slopestyle.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.slopestyle.coderswag.Model.Category
 import com.slopestyle.coderswag.R
+import com.slopestyle.coderswag.Service.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
