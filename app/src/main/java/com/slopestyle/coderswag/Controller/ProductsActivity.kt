dd2e5204b3aaa0a83a0019163234859ec16b37ae
clemenssietas <siecle@gmx.de>
2018-06-20 23:54:00 +0200

    lesson 06

---
+++ app/src/main/java/com/slopestyle/coderswag/Controller/ProductsActivity.kt
@@ -0,0 +1,12 @@
+package com.slopestyle.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
---
+++ app/src/main/java/com/slopestyle/coderswag/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.slopestyle.coderswag.Utilities
+
+class Constants {
+}
